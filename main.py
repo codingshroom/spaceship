1c9e4f287ece306aa143a7e58c9250e20a0393d9
codingshroom <zyt3xx@gmail.com>
2025-06-02 11:20:52 +0200

    moving all directions

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
 import pygame
 from constants import *
-from player import * 
+from player import Player 
 
 
 def main():
     pygame.init()
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-    clock = pygame.time.Clock()
-    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -19,10 +16,11 @@ def main():
                 return
         
         screen.fill("black")
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
-        clock.tick(60)
-        dt = clock.tick() / 1000
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
